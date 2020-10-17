04fba9526c125d995b7e9cc79ab60cf096ef9f7b
Charly Wargnier <cwar05@gmail.com>
2020-10-17 11:19:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 st.header("0")
 
-#classifier = pipeline("zero-shot-classification")
+classifier = pipeline("zero-shot-classification")
 #classifier = pipeline("zero-shot-classification", device=0) # to utilize GPU
 
 df2 = pd.DataFrame(np.array([["boots", 2,3 ], ["boat", 5, 6], ["car", 8, 9]]),columns=['keyword', 'b', 'c'])
