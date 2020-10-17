77c67c0325deb534424d0991c7c5cca8889a68d4
Charly Wargnier <cwar05@gmail.com>
2020-10-17 10:38:41 +0100

    hashed #classifier

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-#from transformers import pipeline
+from transformers import pipeline
 
 import streamlit as st
 
 st.header("0")
 
-classifier = pipeline("zero-shot-classification")
+#classifier = pipeline("zero-shot-classification")
 #classifier = pipeline("zero-shot-classification", device=0) # to utilize GPU
 
 df2 = pd.DataFrame(np.array([["boots", 2,3 ], ["boat", 5, 6], ["car", 8, 9]]),columns=['keyword', 'b', 'c'])
